fd33985b6614882f7c2019f839084f7637ba671a
yogeshdahiya4 <55450030+yogeshdahiya4@users.noreply.github.com>
2021-04-01 10:36:30 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,14 @@
 
+#program - 1
+def area(radius):
+  area = 3.14 * radius * radius
+  return area
+circleRadius = 5
+circleArea = area(circleRadius)
+
+#program - 2
+import os
+
+file = os.path.splitext('my_file.txt')
+file_extension = file[1]
+print("File Extension: ", file_extension)
